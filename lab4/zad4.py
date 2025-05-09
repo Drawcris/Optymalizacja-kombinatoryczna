59058dcb9f0793150c9a55779b8f0bd246382c46
sirwo <sirwoj2@wp.pl>
2025-05-09 19:52:25 +0200

    zad 4

---
+++ lab4/zad4.py
@@ -0,0 +1,27 @@
+from z3 import *
+
+opt = Optimize()
+
+x = Int('x')
+y = Int('y')
+z = Int('z')
+
+# Zakresy dla szybszej pracy
+opt.add(x > y, y > z, z > 0)
+opt.add(x < 200, y < 200, z < 200)
+
+expressions = [x - y, x + z, x - z, y + z, y - z]
+
+for i, expr in enumerate(expressions):
+    s = Int(f'sq_{i}')
+    opt.add(expr == s * s)
+
+opt.minimize(x + y + z)
+
+if opt.check() == sat:
+    m = opt.model()
+    xv, yv, zv = m[x].as_long(), m[y].as_long(), m[z].as_long()
+    print(f"Najmniejsza suma: {xv + yv + zv}")
+    print(f"x = {xv}, y = {yv}, z = {zv}")
+else:
+    print("Brak rozwiązania w danym zakresie.")
